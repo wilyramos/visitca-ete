b00fd7d93d1085c08a2c069eabe412b49f3c4c37
wily ramos <wily.ramos1@unmsm.edu.pe>
2024-05-20 20:30:27 -0500

    modificacion para añadir el branch a github

--- views/paginas/recomendar.php
+++ views/paginas/recomendar.php
@@ -1,7 +1,7 @@
 <main class="paquetes">
     <h2 class="paquetes__heading"><?php echo $titulo ?></h2>
     <p class="paquetes__descripcion"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
-
+    <p class="paquetes__descripcion"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
 
     <?php
 
